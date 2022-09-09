57b7478f7f53c15b66cabee67a9bfa37b968652e
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-09-09 18:23:55 -0400

    first

--- app1.js
+++ app1.js
@@ -1,15 +1,22 @@
+let buttonKim22 = document.getElementById('kim22');
+let i;
+buttonKim22.addEventListener('click', ()=>newFunc(10));
+log();
 
-//"use strict"
-
-
-
-var wolf  = {name: 'wwwwolf'}
-var wolf  = {name: 'wolf'}
-
-for(const value in wolf){
-    dir(wolf[value])
+let x = 0;
+function testWolf(param1 ) {
+  log(`wolfTest: ${i}`);
+  --i;
+  if (i > -2) {
+    testWolf(param1);
+  }
+  return  i;
 }
 
 
-// log(typeof Window)
- dir( globalThis)
+function newFunc(param1 = 5){
+    i = param1
+    log(testWolf(i));
+}
+
+newFunc()
